0e029b9e08025891f92627cdfad5899b31b27d50
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-09-12 11:42:05 +0300

    small refactoring

--- index.js
+++ index.js
@@ -139,10 +139,10 @@ let TocJumper = class {
 	this.data = make_index(this.opt.selector, this.opt.transform)
 	css_inject({ id: this.opt.id })
 	document.body.addEventListener('keydown', (event) => {
-	    if (event.target.nodeName === 'INPUT') return
+	    if (['INPUT', 'TEXTAREA'].indexOf(event.target.nodeName) !== -1)
+		return
 	    if (event.key === this.opt.key && !event.ctrlKey) this.dlg()
-	    // IE11 returns "Esc", Chrome & Firefox return "Escape"
-	    if (event.key.match(/^Esc/)) this.movable._mouseup(null, true)
+	    if (is_escape_key(event)) this.movable._mouseup(null, true)
 	})
     }
 
@@ -187,8 +187,7 @@ let TocJumper = class {
 	node.querySelector(`#${this.opt.id}_close`).onclick = destroy
 	node.addEventListener('keydown', (event) => {
 	    if (event.key === 'Enter') this.scroll(input.value)
-	    // IE11 returns "Esc", Chrome & Firefox return "Escape"
-	    if (event.key.match(/^Esc/)) destroy()
+	    if (is_escape_key(event)) destroy()
 	})
 
 	this.movable = new Movable(node, this.opt.storage_id)
@@ -234,4 +233,9 @@ let css_inject = function(data) {
 
 let focus = function(node) {
     setTimeout( () => node.querySelector('input').focus(), 1)
+}
+
+// IE11 returns "Esc", Chrome & Firefox return "Escape"
+let is_escape_key = function(event) {
+    return event.key.match(/^Esc/)
 }
